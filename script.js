3513adfa753e2b4279f4c05b5daa7d4554dc13bf
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-11-05 00:49:55 +0530

    done

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
 let active = true
-document.querySelector(".store").onclick=()=>{
-    if(active){
+document.querySelector(".store").onclick = () => {
+    if (active) {
         document.querySelector("#s").classList.add('comeup')
         document.querySelector("#s").classList.remove('comedown')
-    document.querySelector("#store").style.display="grid"
-    document.querySelector("#store").classList.remove('fade')
-    document.body.classList.add('fixed')
-active = false}
-else{
-    document.querySelector("#s").classList.remove('comeup')
-    document.querySelector("#s").classList.add('comedown')
-    setTimeout(() => {
+        document.querySelector("#store").style.display = "grid"
+        document.querySelector("#store").classList.remove('fade')
+        document.body.classList.add('fixed')
+        active = false
+    }
+    else {
+        document.querySelector("#s").classList.remove('comeup')
+        document.querySelector("#s").classList.add('comedown')
         document.querySelector("#store").classList.add('fade')
-    }, 400);
-    setTimeout(() => {
-        document.querySelector("#store").style.display="none"
-    }, 500);
-    document.body.classList.remove('fixed')
-    active= true
-}
+        setTimeout(() => {
+            document.querySelector("#store").style.display = "none"
+        }, 1000);
+        document.body.classList.remove('fixed')
+        active = true
+    }
 
 }
 
-document.querySelector("#div").addEventListener('mouseover',()=>{
+document.querySelector("#div").addEventListener('mouseover', () => {
     document.querySelector("#s").classList.remove('comeup')
     document.querySelector("#s").classList.add('comedown')
+    document.querySelector("#store").classList.add('fade')
     setTimeout(() => {
-        document.querySelector("#store").classList.add('fade')
-    }, 400);
-    setTimeout(() => {
-        document.querySelector("#store").style.display="none"
-    }, 500);
+        document.querySelector("#store").style.display = "none"
+    }, 1000);
     document.body.classList.remove('fixed')
-    active= true
+    active = true
 })
 
-if(window.innerWidth <=700) {
+if (window.innerWidth <= 700) {
     let div = document.createElement('p')
     div.innerHTML = "View on Desktop"
     div.id = 'custom'
     div.style.color = "black"
     document.body.appendChild(div)
-}else{
-    document.body.style.display="block"
+} else {
+    document.body.style.display = "block"
 }
